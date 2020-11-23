d90d870840aeda5e4e0b551b8d6d720b09a68b1a
manikcr24 <35169751+manikcr24@users.noreply.github.com>
2020-11-24 02:32:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const PORT = process.env.PORT || 80
 const app = express()
 app.use('/ajira/process', router)
 
+app.get('/', (req, res) => res.sendFile(__dirname+'/src/index.html'))
+
 
 app.listen(PORT, () => console.log(`server running at ${PORT}`))
